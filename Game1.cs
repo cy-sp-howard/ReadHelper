784fc06d4de3fcd51ffc6ca1925079bef03eef38
howard <howard.ye@mxkjtw.com>
2024-09-24 16:32:38 +0800

    init

---
+++ Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ui
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Form _formWrapper;
+        SpriteFont font;
+        public IntPtr FormHandle { get; private set; }
+        public Form Form { get; private set; }
+        bool showForm = false;
+        bool dontHide = false;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            FormHandle = this.Window.Handle;
+            Form = Control.FromHandle(FormHandle).FindForm();
+
+
+            Form.BackColor = System.Drawing.Color.Black;
+            // Avoid the flash the window shows when the application launches (-32000x-32000 is where windows places minimized windows)
+            Form.Location = new System.Drawing.Point(100, 100);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("myfont");
+
+
+            // TODO: use this.Content to load your game content here
+        }
+        protected override void BeginRun()
+        {
+
+            this._graphics.PreferredBackBufferWidth = 200;
+          this._graphics.PreferredBackBufferHeight = 10;
+            this._graphics.ApplyChanges();
+            base.BeginRun();
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            Form.Location = new System.Drawing.Point(100, 100);
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == Microsoft.Xna.Framework.Input.ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.P))
+            {
+
+
+            }
+            // TODO: Add your update logic here
+
+        
+                //{
+
+
+                //    //當窗口的水平垂直尺寸改變時，整個窗口區域都會被重新繪製
+                //    WindowUtil.SetWindowLong(FormHandle, WindowUtil.GWL_STYLE, WindowUtil.CS_HREDRAW | WindowUtil.CS_VREDRAW);
+                //    //WindowUtil.SetLayeredWindowAttributes(FormHandle, 0, 255, 2);
+                //}
+
+                base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(font, "FPS: 123", new Vector2(Window.ClientBounds.Width / 2, 25), Color.Red);
+
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ WindowUtil.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable InconsistentNaming
+
+namespace ui
+{
+
+    internal static class WindowUtil
+    {
+
+
+        public const uint WS_EX_TOPMOST = 0x00000008;
+        public const uint WS_EX_TRANSPARENT = 0x00000020;
+        public const uint WS_EX_TOOLWINDOW = 0x00000080;
+        public const uint WS_EX_CONTROLPARENT = 0x00010000;
+        public const uint WS_EX_APPWINDOW = 0x00040000;
+        public const uint WS_EX_LAYERED = 0x00080000;
+
+        public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+
+        public const uint CS_VREDRAW = 0x0001;
+        public const uint CS_HREDRAW = 0x0002;
+
+        public static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+        public const uint SWP_NOSIZE = 0x0001;
+        public const uint SWP_NOMOVE = 0x0002;
+
+        public const int MINIMIZED_POS = -32000;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ClientToScreen(IntPtr hWnd, ref System.Drawing.Point lpPoint);
+
+        [DllImport("Dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins pMarInset);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetActiveWindow();
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern long GetClassName(IntPtr hwnd, StringBuilder lpClassName, long nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetClientRect(IntPtr hWnd, ref RECT lpRect);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        public static extern IntPtr GetWindowLongPtr32(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+        public static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        public static extern int SetWindowLong32(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        public static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, UIntPtr dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public struct Margins
+        {
+            public int cxLeftWidth;
+            public int cxRightWidth;
+            public int cyTopHeight;
+            public int cyBottomHeight;
+        }
+
+        public enum GW : uint
+        {
+            HWNDFIRST = 0,
+            HWNDLAST = 1,
+            HWNDNEXT = 2,
+            HWNDPREV = 3,
+            OWNER = 4,
+            CHILD = 5,
+            ENABLEDPOPUP = 6
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, GW uCmd);
+
+        internal static void SetForegroundWindowEx(IntPtr handle) => SetForegroundWindow(handle);
+
+        public static IntPtr GetWindowLong(IntPtr hWnd, int nIndex) => IntPtr.Size == 8 ? GetWindowLongPtr64(hWnd, nIndex) : GetWindowLongPtr32(hWnd, nIndex);
+
+        public static int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong) => IntPtr.Size != 8 ? SetWindowLong32(hWnd, nIndex, dwNewLong) : (int)SetWindowLongPtr64(hWnd, nIndex, new UIntPtr(dwNewLong));
+
+        public static void SetWindowParam(IntPtr winHandle, bool showInTaskbar = false)
+        {
+            uint windowParam = WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_CONTROLPARENT | WS_EX_LAYERED;
+
+            if (showInTaskbar)
+            {
+                ShowWindow(winHandle, SW_HIDE);
+                windowParam |= WS_EX_APPWINDOW;
+            }
+            else
+            {
+                windowParam |= WS_EX_TOOLWINDOW;
+            }
+
+            SetWindowLong(winHandle, GWL_EXSTYLE, windowParam);
+
+            if (showInTaskbar)
+            {
+                ShowWindow(winHandle, SW_SHOW);
+            }
+        }
+
+        internal static void SetShowInTaskbar(IntPtr winHandle, bool showInTaskbar)
+        {
+            SetWindowParam(winHandle, showInTaskbar);
+        }
+
+        internal static void SetupOverlay(IntPtr winHandle)
+        {
+            SetWindowLong(winHandle, GWL_STYLE, CS_HREDRAW | CS_VREDRAW);
+
+            // Let Gw2InstanceIntegration sync behavior with game runtime
+            SetShowInTaskbar(winHandle, false);
+
+            SetLayeredWindowAttributes(winHandle, 0, 255, 2);
+        }
+
+        public enum OverlayUpdateResponse
+        {
+            WithFocus,
+            WithoutFocus,
+            Minimized,
+            Errored
+        }
+
+        public static Rectangle pos;
+
+
+    }
+}
